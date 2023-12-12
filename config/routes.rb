118b0719e255c903bc4b2f5c2332fb7eb273c662
Facundo Gasco <facundogasco@gmail.com>
2023-12-12 17:03:19 -0300

    solved

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,26 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get 'tasks', to: "tasks#index"
+  get  "tasks/new", to: "tasks#new",  as: :new_task
+  get  "tasks/:id", to: "tasks#show", as: :task
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: 'tasks#edit', as: :edit_task
+  patch  "tasks/:id",      to: "tasks#update"
 end
+
+# Rails.application.routes.draw do
+#   # Read all
+#   get    "restaurants",          to: "restaurants#index"
+#   # Create
+#   get    "restaurants/new",      to: "restaurants#new",  as: :new_restaurant
+#   post   "restaurants",          to: "restaurants#create"
+#   # Read one - The `show` route needs to be *after* `new` route.
+#   get    "restaurants/:id",      to: "restaurants#show", as: :restaurant
+#   # Update
+#   get    "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+#   patch  "restaurants/:id",      to: "restaurants#update"
+#   # Delete
+#   delete "restaurants/:id",      to: "restaurants#destroy"
+# end
